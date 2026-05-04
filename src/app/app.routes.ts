d50a6d0e1af41c31b713cfb3e9d8da4459588056
lenaStsk <staskevichalena@gmail.com>
2026-05-04 22:03:28 +0200

    feat: add home and about pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+];
--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  private router = inject(Router);
+
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
+
+  navigateToAbout() {
+    this.router.navigate(['/about']);
+  }
+}
