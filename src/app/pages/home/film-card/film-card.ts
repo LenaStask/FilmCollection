ff8a9a9a228482b734d58df00b53c84aad55e10f
lenaStsk <staskevichalena@gmail.com>
2026-05-09 22:01:06 +0200

    feat: implement duration pipe

--- src/app/pages/home/film-card/film-card.ts
+++ src/app/pages/home/film-card/film-card.ts
@@ -1,16 +1,16 @@
 import { Component, inject, input } from '@angular/core';
 import { Film } from '../../../core/models/film.model';
 import { FilmService } from '../../../core/services/film-service';
+import { DurationPipe } from '../../../pipes/duration-pipe';
 
 @Component({
   selector: 'app-film-card',
-  imports: [],
+  imports: [DurationPipe],
   templateUrl: './film-card.html',
   styleUrl: './film-card.scss',
 })
 export class FilmCard {
   film = input.required<Film>();
-
   filmService = inject(FilmService);
 
   toggleFavorite(film: Film) {
